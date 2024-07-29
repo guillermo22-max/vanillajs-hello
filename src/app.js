8c95f75c18389f48a97449540a99d61fc46f3020
guillermo22-max <guillermosoto989@gmail.com>
2024-07-29 15:58:53 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let msj = ["El Lagarto", "Mi Gato", "Mi Perro", "Su Araña"];
+  let accion = ["se comió", "orinó", "aplastó", "rompió"];
+  let cosa = ["mi tarea", "las llaves", "el teléfono"];
+  let cuando = [
+    "antes de la clase",
+    "mientras dormía",
+    "cuando preparaba la comida",
+    "durante mi almuerzo",
+    "mientras me estaba duchando"
+  ];
+
+  function generarExcusaBtn() {
+    const randomWho = msj[Math.floor(Math.random() * msj.length)];
+    const randomAction = accion[Math.floor(Math.random() * accion.length)];
+    const randomWhat = cosa[Math.floor(Math.random() * cosa.length)];
+    const randomWhen = cuando[Math.floor(Math.random() * cuando.length)];
+
+    const excusa = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+    console.log(document.getElementById("excusa"));
+    document.getElementById("excusa").innerHTML = excusa;
+  }
+
+  generarExcusaBtn();
+
+  document
+    .getElementById("generarExcusaBtn")
+    .addEventListener("click", generarExcusaBtn);
+  console.log("Generador de excusas inicializado!");
 };
